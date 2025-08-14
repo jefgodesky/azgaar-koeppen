4110e14e31f1979356ac97200e0c6b05ce9826d7
Jason Godesky <jefgodesky@gmail.com>
2025-08-14 19:02:11 -0400

    Add new type: AzgaarPackCell

---
+++ types/AzgaarPackCell.ts
@@ -0,0 +1,26 @@
+interface AzgaarPackCell {
+  i: number
+  v: number[]
+  c: number[]
+  p: number[]
+  g: number
+  h: number
+  area: number
+  f: number
+  t: number
+  haven: number
+  harbor: number
+  fl: number
+  r: number
+  conf: number
+  biome: number
+  s: number
+  pop: number
+  culture: number
+  burg: number
+  state: number
+  religion: number
+  province: number
+}
+
+export default AzgaarPackCell

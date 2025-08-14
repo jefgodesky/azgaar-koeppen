39ab2c3ed826529debf2ec4d6fa85ea97fffcaa2
Jason Godesky <jefgodesky@gmail.com>
2025-08-14 18:28:08 -0400

    Add new type: World

---
+++ types/World.ts
@@ -0,0 +1,61 @@
+interface World {
+  radius: number // Radius of the planet in kilometers (km)
+  tilt: number // World's axial tilt in degrees.
+  solar: number // Solar constant in Watts per square kilometer (W/m²)
+  months: Record<string, number> // Codes for months, with number of days in each
+  secondsPerDay: number // Number of seconds in a day on this world
+  land: {
+    albedo: number // What fraction of solar radiation is reflected by land?
+    heatCapacity: number // Average land heat capacity in joule per kelvin (J/K)
+  }
+  water: {
+    albedo: number // What fraction of solar radiation is reflected by water?
+    heatCapacity: number // Water's heat capacity in joule per kelvin (J/K)
+  }
+  temperatureDiffusion: number // A fraction reflecting how much temperature diffuses between cells
+  atmosphere: {
+    transmissivity: number // The fraction of solar energy that makes it through.
+    damping: number // Estimate of longwave linear damping
+    intercept: number // How much longwave radiation remains in atmosphere
+  }
+}
+
+export const createWorld = (overrides?: Partial<World>): World => {
+  return {
+    radius: 6371,
+    tilt: 23.44,
+    solar: 1361,
+    months: {
+      Jan: 31,
+      Feb: 28,
+      Mar: 31,
+      Apr: 30,
+      May: 31,
+      Jun: 30,
+      Jul: 31,
+      Aug: 31,
+      Sep: 30,
+      Oct: 31,
+      Nov: 30,
+      Dec: 31
+    },
+    secondsPerDay: 24 * 60 * 60,
+    land: {
+      albedo: 0.3,
+      heatCapacity: 1e7
+    },
+    water: {
+      albedo: 0.06,
+      heatCapacity: 8e7
+    },
+    temperatureDiffusion: 0.1,
+    atmosphere: {
+      transmissivity: 0.5,
+      damping: 5.6,
+      intercept: 0
+    },
+    ...overrides
+  }
+}
+
+export default World

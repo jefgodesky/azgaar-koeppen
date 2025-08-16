86813382b4da2074f0461c6f172c6a294383a544
Jason Godesky <jefgodesky@gmail.com>
2025-08-15 21:15:48 -0400

    Add new utility method: findRegion

--- types/Cell.ts
+++ types/Cell.ts
@@ -27,4 +27,18 @@ export const createCell = (overrides?: Partial<Cell>): Cell => {
   }
 }
 
+export const createTinyWorld = (): Record<number, Cell> => {
+  return {
+    1: createCell({ id: 1, neighbors: [2, 4] }),
+    2: createCell({ id: 2, neighbors: [1, 3, 5] }),
+    3: createCell({ id: 3, neighbors: [2, 6] }),
+    4: createCell({ id: 4, neighbors: [1, 5, 7] }),
+    5: createCell({ id: 5, neighbors: [2, 6, 8, 4] }),
+    6: createCell({ id: 6, neighbors: [3, 5, 9] }),
+    7: createCell({ id: 7, neighbors: [4, 8] }),
+    8: createCell({ id: 8, neighbors: [5, 7, 9] }),
+    9: createCell({ id: 9, neighbors: [6, 8] })
+  }
+}
+
 export default Cell

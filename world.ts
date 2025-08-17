83e72ad35f351ae5cee4d7990fdd8b4b9da870d4
Jason Godesky <jefgodesky@gmail.com>
2025-08-17 15:40:07 -0400

    Export world
    This is the world that's actually exported to scripts, giving users a single place where they can customize the parameters of their own worlds.

---
+++ world.ts
@@ -0,0 +1,9 @@
+import { createWorld } from './types/World.ts'
+
+/**
+ * This provides the world that is actually used by scripts. See
+ * ./types/World.ts for documentation on the options that you can pass as part
+ * of the object given to `createWorld` to customize your world.
+ */
+
+export default createWorld({})
